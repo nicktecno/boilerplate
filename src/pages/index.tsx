51106d19ff6c0a37f7c7d6627cb99c638a1c0a94
nicktecno <nick.tecno@gmail.com>
2021-08-21 12:03:45 -0300

    feat: first commitsss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const bls = 'var'
+  const bls = 'vara'
 
   return (
     <div className={styles.container}>
